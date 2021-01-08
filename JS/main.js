e5ae625086e4e768eb1ede84b3699cac554842c9
Mircastweb <49751380+mircastweb@users.noreply.github.com>
2021-01-08 11:57:34 +0100

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+const photoContainer = document.getElementById("photo-container");
+
+let content= "<div class='column'>";
+
+for(let i=0; i<20; i++){
+    
+    if(i!=0 && (i%5==0)){
+        content += `</div><div class="column">
+       ${getPhotoElement(i+1)} `
+    }else{
+        content += getPhotoElement(i+1);
+    }
+}
+
+content += "<div/>" ;
+
+photoContainer.innerHTML=content;
+
+function getPhotoElement(photo_id){
+    return `<a href="#" class="photo">
+    <img alt="Photo ${photo_id}" src= "/img/photo_${photo_id}.jpg"/>
+    </a>` ;
+}
